62b24dfbfcb4f0184edc8a8e4d993a0b03c6d412
gerssivaldosantos <canalandows@gmail.com>
2022-01-27 04:45:56 -0300

    formating the value and description

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ var list = [
     { "description": "rice", "amount": "1", "value": "5.40" },
     { "description": "beer", "amount": "12", "value": "1.99" },
     { "description": "meat", "amount": "1", "value": "15.00" },
+    { "description": "chicken", "amount": "4", "value": "11.00" },
 ]
 
 function getTotal(list) {
@@ -13,24 +14,40 @@ function getTotal(list) {
 }
 
 function setList(list) {
-    
-    var table = '<thead>< tr >'+
+
+    var table = '<thead><tr>'+
     '<td>Description</td>'+
     '<td>Amount</td>'+
     '<td>Value</td>'+
-    '<td>Action</td></tr ></thead > ';
+    '<td>Action</td></tr></thead > ';
 
     for (var key in list){
         table += '<tbody><tr><td>'
-        +list[key].description+
+        +formatDesc(list[key].description)+
         '</td><td>'
         +list[key].amount+
         '</td><td>'
-        +list[key].value+
+        +formatValue(list[key].value)+
         '</td><td>Edit | Delete</td></tr>'
          
     }
     table += '</tbody>'
+    document.getElementById('listTable').innerHTML = table;
 }
 
-console.log(getTotal(list));
+function formatDesc(desc){
+    var str = desc.toLowerCase();
+    /* Setting the first letter to upper case */
+    str = str.charAt(0).toUpperCase() + str.slice(1);
+    return str;
+}
+
+function formatValue(value){
+    var str = parseFloat(value).toFixed(2) +"";
+    str = str.toString();
+    str = str.replace('.',',');
+    str = "R$ "+str;
+    return str;
+}
+
+setList(list);
