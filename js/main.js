2ae4bd24e330f342af64ea1d2a8e6284916c2124
gerssivaldosantos <canalandows@gmail.com>
2022-01-27 04:27:53 -0300

    function to setList

--- js/main.js
+++ js/main.js
@@ -4,12 +4,33 @@ var list = [
     { "description": "meat", "amount": "1", "value": "15.00" },
 ]
 
-function getTotal(list){
+function getTotal(list) {
     var total = 0
-    for (var i in list){
+    for (var i in list) {
         total += list[i].value * list[i].amount;
     }
     return total;
+}
+
+function setList(list) {
+    
+    var table = '<thead>< tr >'+
+    '<td>Description</td>'+
+    '<td>Amount</td>'+
+    '<td>Value</td>'+
+    '<td>Action</td></tr ></thead > ';
+
+    for (var key in list){
+        table += '<tbody><tr><td>'
+        +list[key].description+
+        '</td><td>'
+        +list[key].amount+
+        '</td><td>'
+        +list[key].value+
+        '</td><td>Edit | Delete</td></tr>'
+         
+    }
+    table += '</tbody>'
 }
 
 console.log(getTotal(list));
